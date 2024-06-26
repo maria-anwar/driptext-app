4066f7f21bd0231a1e2da35469869d7fbc71e9d7
Maria Anwar <mariaanwar996@gmail.com>
2024-06-26 19:44:10 +0500

    header component added

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import Footer from '../views/Footer';
+import Header from '../components/Header';
 
 const HomeLayout = () => {
   return (
     <>
     <div>
+      <Header/>
       <div>
         <Outlet />
       </div>
