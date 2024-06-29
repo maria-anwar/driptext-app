dff6b8864f565390d323d317cd8d3fa3072b2098
Maria Anwar <mariaanwar996@gmail.com>
2024-06-29 08:32:11 +0500

    buttons links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,9 +11,9 @@ const Header = () => {
           </div>
         </div>
         <div>
-          <button className="py-4 px-16 bg-custom-yellow text-dark-blue text-xl font-bold rounded-lg hover:bg-yellow-600 transition duration-200">
+          <Link to='/register/form' className="py-4 px-16 bg-custom-yellow text-dark-blue text-xl font-bold rounded-lg hover:bg-yellow-600 transition duration-200">
             Order Free Sample Text
-          </button>
+          </Link>
         </div>
       </header>
     </>
