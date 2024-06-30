f63678e28f20c281474cc603f82f4ccc33ce957e
Maria Anwar <mariaanwar996@gmail.com>
2024-06-30 13:27:42 +0500

    hero section

--- src/components/landing-page/HeroSection.jsx
+++ src/components/landing-page/HeroSection.jsx
@@ -6,7 +6,7 @@ import img3 from "../../assets/homeimages/DripText-Features-2.png";
 import { Link } from "react-router-dom";
 const HeroSection = () => {
   return (
-    <div className="w-full flex justify-center px-1 xxs:px-2  xs:px-3 sm:px-5 md:px-6  lg:px-7 xl:px-9 2xl:px-14 3xl:px-20 4xl:px-52">
+  
       <div className="w-full flex flex-col gap-10 xs:gap-14 justify-center items-center bg-dark-blue rounded-xl 4xl:rounded-2xl  mt-4 border px-3 py-6 xxs:px-8 xxs:py-10 xs:px-7 xs:py-20">
         <div className="w-full grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 justify-center items-center px-5 xxs:px-4">
           <div className="w-full flex flex-col gap-1 justify-center items-center">
@@ -91,7 +91,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-    </div>
+  
   );
 };
 
--- src/views/index.jsx
+++ src/views/index.jsx
@@ -4,7 +4,9 @@ import HeroSection from '../components/landing-page/HeroSection'
 const Home = () => {
   return (
    <>
-   <HeroSection />
+   <div className='w-full flex justify-center px-1 xxs:px-2  xs:px-3 sm:px-5 md:px-6  lg:px-7 xl:px-9 2xl:px-14 3xl:px-20 4xl:px-52'>
+      <HeroSection />
+   </div>
    </>
   )
 }
