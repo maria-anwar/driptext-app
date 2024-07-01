51919644fcb77ff20336bfe7d2cfe9238ae5b8f0
Maria Anwar <mariaanwar996@gmail.com>
2024-07-02 02:54:10 +0500

    payment card issue resolved

--- src/components/home/PaymentSection/Payment.jsx
+++ src/components/home/PaymentSection/Payment.jsx
@@ -203,11 +203,11 @@ const PricingSection = () => {
           <PricingCard key={index} plan={plan} />
         ))}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {plansForTexts[selectedTexts].map((plan, index) => (
           <PricingCard key={index} plan={plan} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
