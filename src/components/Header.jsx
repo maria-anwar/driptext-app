b2d2b054af66da41e09a304db9665afe33849f58
Muhammad Ahmed <muhhmadahmad91@gmail.com>
2024-07-01 05:56:36 +0500

    colour correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <header className="w-full flex justify-between items-center px-1 xxs:px-2 xs:px-3 sm:px-5 md:px-6 lg:px-7 xl:px-9 2xl:px-14 3xl:px-20 4xl:px-52 py-4 ">
       <div className="flex items-center">
-        <img src={Logo} alt="Logo" className="h-6 xs:h-8 sm:h-10 md:h-12 lg:h-14" />
+        <img src={Logo} alt="Logo" className="xxs:h-6 lg:h-8 " />
       </div>
       <div>
         <Link
           to="/register/form"
-          className="py-4 px-2 xs:py-2 xs:px-4 sm:py-2 sm:px-5 md:py-2 md:px-6 lg:py-6 lg:px-8 xl:py-4 bg-gradient-to-r from-sky-300 to-sky-600 text-xs xs:text-sm sm:text-base md:text-lg lg:text-xl text-white font-bold rounded-lg"
+          className="py-4 px-2 xs:py-2 xs:px-4 sm:py-2 sm:px-5 md:py-2 md:px-6 lg:py-6 lg:px-8 xl:py-4 bg-gradient-to-r from-sky-700 to-cyan-500 text-xs xs:text-sm sm:text-base md:text-lg lg:text-xl text-white font-bold rounded-lg"
         >
           Order Free Sample Text
         </Link>
