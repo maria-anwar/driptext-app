f04f16e70aa6057df2b4cad777b49b9848f4ef91
zamanwebhunters <zaman@thewebhunters.com>
2024-07-26 02:38:24 +0500

    start redux implementation

---
+++ src/redux/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import userReducer from '../features/user/userSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'], //  all here will presiste
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+ 
