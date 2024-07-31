84e04572ea3a9c313977bf834fcc14152c746e3d
abubakarnangrii <abubakarnangri@gmail.com>
2024-08-01 00:15:00 +0500

    earning table changes are made

---
+++ src/components/freelancer/breeadcrumbs/Breadcrumb.tsx
@@ -0,0 +1,31 @@
+import React, { Link } from 'react-router-dom';
+interface BreadcrumbProps {
+  pageName: string;
+}
+const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+  return (
+    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <div className="w-full 2xl:max-w-max">
+          <h1 className="text-title-md font-bold text-black dark:text-white mb-2">
+           {pageName}
+          </h1>
+          <p className="text-dark-gray">
+            Here you can see all the earning from all your DripTexts projects.
+          </p>
+        </div>
+
+      <nav>
+        <ol className="flex items-center gap-2">
+          <li>
+            <Link className="font-medium" to="/freelancer-dashboard">
+              Dashboard /
+            </Link>
+          </li>
+          <li className="font-medium text-primary">{pageName}</li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
+
+export default Breadcrumb;
